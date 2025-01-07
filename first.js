b0c157929e7d81a744cf738c0c4d6bf5063e343e
developer9560 <developer95608@gmail.com>
2025-01-07 15:46:03 +0530

    i made box

--- first.js
+++ first.js
@@ -1,2 +1,19 @@
-console.log("hello world");
-console.log("suraj kumar you are best");
+let div = document.createElement("div");
+
+div.style.width = "500px";
+div.style.height = "500px";
+div.style.backgroundColor= "lightblue";
+
+
+let body = document.querySelector("body");
+body.style.backgroundColor = "gray";
+body.appendChild(div);
+for(let i = 1; i<=9; i++){
+   let divs = document.createElement("div");
+    divs.setAttribute("class", "box");
+    div.appendChild(divs);
+}
+
+
+
+
